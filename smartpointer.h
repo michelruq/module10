e3b29f6be689110ccbbea4c6a06850b34cb00f8d
Mikhail <Mikhail.Chashchin@emerson.com>
2023-12-22 23:36:10 +0500

    fifth iteration of smart point

--- smartpointer.h
+++ smartpointer.h
@@ -16,6 +16,12 @@ public:
 		*m_ptr = *a.m_ptr;
 	}
 
+	Auto_ptr1(Auto_ptr1&& a) noexcept 
+		: m_ptr{ a.m_ptr }
+	{
+		a.m_ptr = nullptr;
+	}
+
 	Auto_ptr1& operator=(const Auto_ptr1& a)
 	{
 		if (&a == this)
@@ -26,6 +32,21 @@ public:
 		delete m_ptr;
 		m_ptr = new T;
 		*m_ptr = *a.m_ptr;
+		return *this;
+	}
+
+	Auto_ptr1& operator=(Auto_ptr1&& a)
+	{
+		if (&a == this)
+		{
+			return *this;
+		}
+
+		delete m_ptr;
+
+		m_ptr = a.m_ptr;
+		a.m_ptr = nullptr;
+
 		return *this;
 	}
 
